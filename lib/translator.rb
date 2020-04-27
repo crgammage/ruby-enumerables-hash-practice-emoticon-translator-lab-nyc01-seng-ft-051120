f5b92a79afb8464dc5581fa238c49b05c19d8423
Christa Gammage <crgammage@gmail.com>
2020-04-27 00:39:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,19 +11,15 @@ def load_library(file)
   emoticons
 end
 
-# def get_japanese_emoticon(file, emoticon)
-#   # emoticons = load_library(file)
-#   # load_library(file)
-# emoticons = library_load(file)
-#   # japanese_emoticon = load_library(file)["get_emoticon"][emoticon]
-# if emoticons[feeling][:english].find(emoticon)
-#     return emoticons[feeling][:japanese]
-#   end
-#   binding.pry
-#   # else
-#   #   returns "That is not a known emoticon."
-#   # end
-# end
+def get_japanese_emoticon(file, emoticon)
+  emoticons = load_library(file)
+  emoticons.keys.find do |key|
+    if emoticon == emoticons[key][:english]
+      return key
+    end
+  end
+    return "Sorry, that emoticon was not found"
+end
 
 def get_english_meaning(file, emoticon)
   emoticons = load_library(file)
